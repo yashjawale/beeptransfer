9ab76a89471648332649d4eae7ce26232bf3f9f7
yashjawale <yash25.j@gmail.com>
2026-03-09 00:04:54 +0530

    add beep function

---
+++ beeps.py
@@ -0,0 +1,23 @@
+import sounddevice as sd
+import numpy as np
+import time
+
+sample_rate = 44100
+dot_duration = 0.1
+
+def beep(type):
+    duration = dot_duration if type == "dot" else dot_duration * 3
+    
+    # Generate numpy array for time duration
+    t = np.linspace(0, duration, int(sample_rate * duration), False)
+
+    # Create beep sound as sine wave
+    # Sine wave formule -> Amplitude * sin(2 * pi * frequency * time)
+    beep = 0.5 * np.sin(2 * np.pi * 800 * t)
+    
+    # Play beep and wait for it to finish
+    sd.play(beep, sample_rate)
+    sd.wait() 
+    
+    # Silence for one dot duration
+    time.sleep(dot_duration)
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from morse import morse_code
+from beeps import beep
 
 def main():
     print(morse_code["a"])
+    beep("dash")
+    beep("dot")
+    beep("dot")
+    beep("dash")
 
 
 if __name__ == "__main__":
