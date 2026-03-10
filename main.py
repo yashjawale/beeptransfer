003fa285cbdd4681525a6600e042215add790162
yashjawale <yash25.j@gmail.com>
2026-03-10 13:52:52 +0530

    beep out message with prompt

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from morse import convert_text_to_morse, convert_morse_to_char
 from beeps import beep_morse
+from rich.prompt import Prompt
 
 def main():
+    choice = Prompt.ask("Choose mode of program", choices=["send", "listen"], default="send")
     
-    morse = convert_text_to_morse("This is Cs50")
-    beep_morse(morse)
+    if choice == "send":
+        text = Prompt.ask("Enter your message")
+        print("Beeping message...")
+        morse = convert_text_to_morse(text)
+        
+        beep_morse(morse)
 
 
 if __name__ == "__main__":
