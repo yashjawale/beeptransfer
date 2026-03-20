636f176e3934a162f39b1d27a134a2bb54aa40fb
yashjawale <yash25.j@gmail.com>
2026-03-20 22:27:55 +0530

    sleep between each letter too

--- beeps.py
+++ beeps.py
@@ -1,29 +1,32 @@
-import sounddevice as sd
-import numpy as np
 import time
+
+import numpy as np
+import sounddevice as sd
 
 sample_rate = 44100
 dot_duration = 0.1
 
+
 def beep(type):
-    
+
+    # After each word is complete
     if type == "WAIT":
         time.sleep(dot_duration * 7)
         return
-    
+
     duration = dot_duration if type == "dot" else dot_duration * 3
-    
+
     # Generate numpy array for time duration
     t = np.linspace(0, duration, int(sample_rate * duration), False)
 
     # Create beep sound as sine wave
     # Sine wave formule -> Amplitude * sin(2 * pi * frequency * time)
     beep = 0.5 * np.sin(2 * np.pi * 800 * t)
-    
+
     # Play beep and wait for it to finish
     sd.play(beep, sample_rate)
-    sd.wait() 
-    
+    sd.wait()
+
     # Silence for one dot duration
     time.sleep(dot_duration)
 
@@ -38,3 +41,6 @@ def beep_morse(morse):
                     beep("dot")
                 elif char == "-":
                     beep("dash")
+
+            # Sleep between letters
+            time.sleep(dot_duration * 3)
