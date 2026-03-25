9f85c5e3dd52707e4698ff510c0a6fcf52c7acc0
yashjawale <yash25.j@gmail.com>
2026-03-25 14:04:19 +0530

    new line after complete

--- project.py
+++ project.py
@@ -129,7 +129,7 @@ def audio_callback(indata, frames, time, status):
             high = 0
     
     if letter == "...---...":
-        print("----------------")
+        print("\n----------------")
         print("COMPLETE")
         raise sd.CallbackStop()
 
